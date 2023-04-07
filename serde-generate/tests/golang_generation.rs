d7805ff25044568a42b9c977243a9695c7c964fc
Gerardo Di Giacomo <19227040+gedigi@users.noreply.github.com>
2023-04-06 18:39:48 -0700

    fix python code and golang modules/tests

--- serde-generate/tests/golang_generation.rs
+++ serde-generate/tests/golang_generation.rs
@@ -69,7 +69,7 @@ fn test_that_golang_code_compiles_with_config_and_registry(
         .arg("edit")
         .arg("-replace")
         .arg(format!(
-            "github.com/novifinancial/serde-reflection/serde-generate/runtime/golang={}",
+            "github.com/zefchain/serde-reflection/serde-generate/runtime/golang={}",
             runtime_mod_path.to_str().unwrap()
         ))
         .status()
